a8df3a68b9df1bbb0b8e1766daf4c9a7df7642fc
Aakash Khatter <aakashkhatter1296@gmail.com>
2016-04-09 23:48:57 +0530

    Added fab button for new deal & product

--- AndroidStudioProjects/Spotlight/SpotlightShop/app/src/main/java/android/spotlightshop/com/spotlightshop/MainActivity.java
+++ AndroidStudioProjects/Spotlight/SpotlightShop/app/src/main/java/android/spotlightshop/com/spotlightshop/MainActivity.java
@@ -1,15 +1,19 @@
 package android.spotlightshop.com.spotlightshop;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton productFab;
+    FloatingActionButton dealFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +22,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        dealFab = (FloatingActionButton) findViewById(R.id.fab_deal);
+        productFab = (FloatingActionButton) findViewById(R.id.fab_product);
+
+        productFab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), NewProductActivity.class));
             }
         });
+
+        dealFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), NewDealActivity.class));
+            }
+        });
+
+
     }
 
     @Override
